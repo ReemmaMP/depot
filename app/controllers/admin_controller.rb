c8aee29f4be951b8588b90e30a837da5da5f772d
Reemma Puredath <reemma.puredath@deliveroo.co.uk>
2017-06-28 11:00:40 +0100

    restricted access for admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
+  before_action :authorize
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  
+
+    end
   def index
-    
+
   end
 end
